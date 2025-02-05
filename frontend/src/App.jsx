5de9937b3311c069fbb7f7732353ae290906af59
Regiane <regianemelo1995@gmail.com>
2025-02-05 14:16:37 -0300

    configurando react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 
+import { Outlet } from 'react-router-dom'
+
 function App() {
 
   return (
       <div className='App'>
         <h1>Memories</h1>
+        <div className="container">
+          <Outlet />
+        </div>
       </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Pages
+import AddMemory from './routes/AddMemory.jsx'
+import Home from './routes/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {path: "/", element: <Home /> },
+      {path: "/add-memory", element: <AddMemory />}
+    ],
+  },
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
