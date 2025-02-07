fd8289b58bcdea0e3bb27637429a408b5d7c4bd0
Regiane <regianemelo1995@gmail.com>
2025-02-07 15:53:19 -0300

    Resgatando memoria da home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from "../axios-config"
+
+// import { Link } from 'react-router-dom'
+
+import './Home.css'
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const [memories, setMemories] = useState([])
+
+  useEffect(() => {
+    const getMemories = async() => {
+      const res = await axios.get("/memories")
+
+      setMemories(res.data)
+    }
+
+    getMemories()
+  }, [])
+
+  return <div className='home'>
+      <h2>Confira as últimas memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 && memories.map((memory) => (
+          <p>{memory.title}</p>
+        ))}
+      </div>
+    </div>
 }
 
 export default Home
