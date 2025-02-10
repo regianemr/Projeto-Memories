b3f59d9a4ecc4898349954caf3eb0d8f8bd0b8d6
Regiane <regianemelo1995@gmail.com>
2025-02-10 11:40:10 -0300

    css da home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from "../axios-config"
 
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './Home.css'
 
@@ -22,7 +22,11 @@ const Home = () => {
       <h2>Confira as últimas memórias</h2>
       <div className="memories-container">
         {memories.length > 0 && memories.map((memory) => (
-          <p>{memory.title}</p>
+          <div className='memory' key={memory._id}>
+            <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+            <p>{memory.title}</p>
+            <Link className='btn' to={`/memories/${memory._id}`}>Comentar</Link>
+          </div>
         ))}
       </div>
     </div>
